2e2cb9e6dfa7b60eab594a43ba0b2400ed0a4643
Riddhi Kakkad <riddhikakkad.2610@gmail.com>
2024-06-19 16:04:16 +0530

    changes

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,13 @@
+import Hero from "@/components/main/Hero";
 import Image from "next/image";
 
 export default function Home() {
   return (
-    <main className='h-full w-full'>
-      <div className='flex flex-col gap-20'>
-
+    <main className="h-full w-full">
+      <div className="flex flex-col h-[850px] gap-20">
+      <Hero />
       </div>
       
     </main>
-  )
+  );
 }
--- components/main/Hero.tsx
+++ components/main/Hero.tsx
@@ -2,17 +2,17 @@ import React from 'react'
 
 const Hero = () => {
   return (
-    <div className='relative flex flex-col h-full w-full'>
+    <div className="relative flex flex-col h-full w-full">
         <video 
         autoPlay
         muted
         loop
-        className='rotate-180 absolute top-[-340px] left-0 z-[1] w-full h-full object-cover'
+        className="rotate-180 absolute top-[-340px] h-full w-full left-0 z-[1] object-cover"
         >
-            <source src='/blackhole.webm' type='video/webm' />
+            <source src="/blackhole.webm" type="video/webm" />
         </video>
     </div>
-  )
-}
+  );
+};
 
-export default Hero
+export default Hero;
