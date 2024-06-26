fec8f2da81dab9a310f69130988ec17348853e6e
Riddhi Kakkad <riddhikakkad.2610@gmail.com>
2024-06-26 23:14:54 +0530

    changes

--- app/layout.tsx
+++ app/layout.tsx
@@ -5,7 +5,7 @@ import "./globals.css";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: "Space Portfolio",
+  title: "Space Portfoliooooo",
   description: "this is my portfolio",
 };
 
