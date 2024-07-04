4faf81cc5746eb8c547bf8cdd7b70744d04ffa94
Riddhi Kakkad <riddhikakkad.2610@gmail.com>
2024-07-04 22:51:23 +0530

    changes

--- components/main/Navbar.tsx
+++ components/main/Navbar.tsx
@@ -21,7 +21,7 @@ const Navbar = () => {
             <div className='flex item-center justify-between border border-[#7042f861] bg-[#0300145e] mr-[15px] px-[20px] py-[10px] rounded-full text-gray-200'>
             <a href="#about-me" className='cursor-pointer'>About me</a>
             <a href="#skills" className='cursor-pointer'>Skills</a>
-            <a href="#projects" className='cursor-pointer'>My Projects</a>
+            <a href="#projects" className='cursor-pointer'>Projects</a>
 
             </div>
         </div>
