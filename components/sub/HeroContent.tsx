5ba7e0c21314828340f6deed37822b2be16d2a16
Riddhi Kakkad <riddhikakkad.2610@gmail.com>
2024-07-11 22:38:43 +0530

    changes

--- components/sub/HeroContent.tsx
+++ components/sub/HeroContent.tsx
@@ -24,7 +24,7 @@ const HeroContent = () => {
         >
           <SparklesIcon className="text-[#b49bff] mr-[10px] h-5 w-5" />
           <h1 className="Welcome-text text-[13px]">
-            Hi, welcome to Fullstack Developer Portfolio
+            Hi, welcome to my Portfolio
           </h1>
         </motion.div>
 
