c7be5267cae2515a9f8a67aac9eb2bf3364efbd2
Riddhi Kakkad <riddhikakkad.2610@gmail.com>
2024-07-15 22:41:04 +0530

    changes

--- app/layout.tsx
+++ app/layout.tsx
@@ -9,7 +9,7 @@ const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
   title: "Riddhi Kakkad",
-  description: "This is my portfolio",
+  description: "This is my dynamic portfolio",
 };
 
 export default function RootLayout({
